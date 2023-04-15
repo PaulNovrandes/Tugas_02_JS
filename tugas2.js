075e747196ee4e4ec1f44a1e4617285c4cb49d95
PaulNovrandes <novrandes.tanuma@gmail.com>
2023-04-15 13:19:56 +0700

    Tugas 02 JS
    Tugas ini dikumpulkan kepada niomic

---
+++ tugas2.js
@@ -0,0 +1,11 @@
+var number1 = 10;
+var nama = "Paul Novrandes";
+var pria = true;
+var data;
+number2 = null;
+
+console.log(number1);
+console.log(nama);
+console.log(pria);
+console.log(data);
+console.log(number2);
